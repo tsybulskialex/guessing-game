c6e91a8e6536d72607e22d9dd115369e59585766
Aleksandr <tsybulski.alex@mail.ru>
2024-09-24 22:34:39 +0300

    feat: solution of task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.maxValue = null;
+        this.minValue = null;
+        this.middle = null;
+
+    }
 
     setRange(min, max) {
-
+        this.maxValue = max;
+        this.minValue = min;
     }
 
     guess() {
-
+        this.middle = Math.round((this.minValue + (this.maxValue - this.minValue) / 2));
+        return this.middle;
     }
 
     lower() {
-
+        this.maxValue = this.middle;
     }
 
     greater() {
-
+        this.minValue = this.middle;
     }
 }
 
